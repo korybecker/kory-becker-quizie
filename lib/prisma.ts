b105766cef2bed4f37926fde00c40d75d4e7e307
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-08 21:14:45 -0500

    changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from '../node_modules/.prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     experimental: {
         appDir: true,
-        serverComponentsExternalPackages: ["@prisma/client"],
+        serverComponentsExternalPackages: ["prisma"],
     },
 };
 
