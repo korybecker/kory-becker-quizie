3dc56691ad12b646449fc08bcbb15127139dfd07
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-21 12:23:10 -0500

    added delete api route for deleting sound

---
+++ pages/api/delete.js
@@ -0,0 +1,57 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async (req, res) => {
+    const { quizId } = req.body;
+
+    // console.log(quizId);
+
+    try {
+        await prisma.answer.deleteMany({
+            where: {
+                option: {
+                    question: {
+                        quizId: quizId,
+                    },
+                },
+            },
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
+    try {
+        await prisma.option.deleteMany({
+            where: {
+                question: {
+                    quizId: quizId,
+                },
+            },
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
+    try {
+        await prisma.question.deleteMany({
+            where: {
+                quizId: quizId,
+            },
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
+    try {
+        await prisma.quiz.delete({
+            where: {
+                id: quizId,
+            },
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
+    res.status(204).json({ msg: "delete success" });
+};
